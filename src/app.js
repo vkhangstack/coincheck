bb55160d2edb12339c7e333bfd1af303eee48d97
Pham Van Khang <phamvankhang.tvi@gmail.com>
2021-10-07 06:13:14 +0700

    define command line symbol

--- src/app.js
+++ src/app.js
@@ -16,3 +16,35 @@ const getColoredChangeValue = (value) => {
   const text = `${value}%`;
   return (value && (value > 0 ? text.green : text.red)) || "NA";
 };
+
+const getValidTop = (top) => {
+  if (Number.isNaN(top) || top < 1) {
+    return DEFAULT_TOP;
+  }
+  if (top > MAX_TOP) {
+    return MAX_TOP;
+  }
+  return top;
+};
+// command line interface
+const { version } = require("../package.json");
+process
+  .version(version)
+  .option(
+    "-f, --find [symbol]",
+    "Find specific coin data with coin symbol (can be a command separators list",
+    list,
+    [],
+  )
+  .option(
+    "-t, --top [index]",
+    "Show the top coins ranked from 1 - [index] according to the market cap",
+    validate.validateNumber,
+    DEFAULT_TOP,
+  )
+  .parse(process.argv);
+
+console.log("\n");
+
+const find = program.find;
+const top = find.length > 0 ? MAX : getValidTop(program.top);
